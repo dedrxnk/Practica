22d5d7634d20d7dc5ac03ec9132e6d4d224bb97e
dedrxnk <84861063+dedrxnk@users.noreply.github.com>
2021-05-26 20:36:39 +0500

    Update Практика6.cs

--- Практика6.cs
+++ Практика6.cs
@@ -1,102 +1,173 @@
+  
 using System;
-using System.Collections;
-using System.IO;
-
+using System.Collections.Generic;
+using studentlib;
 namespace Practica6
-
 {
     class Program
     {
-        public class Student
-        {
-            public string Age { get; set; }
-            public string FIO { get; set; }
-            public string Gruppa { get; set; }
-            public override string ToString()
-            {
-                return string.Format("ФИО:{0},Группа:{1}, Возраст:{2}", FIO, Gruppa, Age);
-            }
-        }
-
         static void Main(string[] args)
         {
-            bool kones = true;
-            ArrayList al = new ArrayList();
-
-            while (kones)
+            try
             {
-                Console.WriteLine("Меню");
-                PrintMessage();
-                int a = int.Parse(Console.ReadLine());
-                if (a == 1)
+                bool exit = false;
+                while (true)
                 {
-                    AddStudent(al);
+                    int Choice = 0;
+                    Console.WriteLine("'1' - Вся информацию о студентах.\n'2' - Список студентов по инициалам\n'3' - Список студентов старше 18\n'4' - Список студентов младше 18\n'5' - Добавить студента\n'6' - Удалить студента\n'7' - Изменить студента\n'8' - Поиск студентов\n'9' - Выход");
+                    while (true)
+                    {
+                        Console.WriteLine("Выберите номер: ");
+                        Choice = Convert.ToInt32(Console.ReadLine());
+                        if (Choice > 10 || Choice < 1)
+                        {
+                            Console.WriteLine("Такого номера нет");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (Choice == 1)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов");
+                        }
+                        else
+                        {
+                            foreach (Student student in Student.ListStudent)
+                            {
+                                Student.ExitID(student.IDStudent);
+                            }
+                        }
+                    }
+                    else if (Choice == 2)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов");
+                        }
+                        else
+                        {
+                            Student.DeleteAllStudent();
+                        }
+                    }
+                    else if (Choice == 3)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            foreach (Student student in Student.ListStudent)
+                            {
+                                if (student.HeightStudent(student.IDStudent) >= 18)
+                                {
+                                    Student.ExitID(student.IDStudent);
+                                }
+                            }
+                        }
+                    }
+                    else if (Choice == 4)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            foreach (Student student in Student.ListStudent)
+                            {
+                                if (student.HeightStudent(student.IDStudent) <= 18)
+                                {
+                                    Student.ExitID(student.IDStudent);
+                                }
+                            }
+                        }
+                    }
+                    else if (Choice == 5)
+                    {
+                        Console.WriteLine("Введите ФИО студента: ");
+                        String FIO = Console.ReadLine().Trim();
+                        Console.WriteLine("Введеите группу студента: ");
+                        String Group = Console.ReadLine().Trim();
+                        Console.WriteLine("Дата рождения: ");
+                        Console.WriteLine("Введите день: ");
+                        String Day = Console.ReadLine();
+                        Console.WriteLine("Введите месяц: ");
+                        String Month = Console.ReadLine();
+                        Console.WriteLine("Введите год: ");
+                        String Year = Console.ReadLine();
+                        Student stud = new Student(FIO, Group, new DateTime(Convert.ToInt32(Year), Convert.ToInt32(Month), Convert.ToInt32(Day)));
+                        Student.AddStudent(stud);
+                        Console.WriteLine("Студент добавлен");
+                    }
+                    else if (Choice == 6)
+                    {
+                        Console.WriteLine("Введеите id для удаления: ");
+                        int DeleteID = Convert.ToInt32(Console.ReadLine());
+                        Student.DeleteStudent(DeleteID);
+                        Console.WriteLine("Студент удалён");
+                    }
+                    else if (Choice == 7)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введете id для изменения студента: ");
+                            int idEdit = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите ФИО студента: ");
+                            String fioEdit = Console.ReadLine().Trim();
+                            Console.Write("Введеите группу студента: ");
+                            String groupEdit = Console.ReadLine().Trim();
+                            Console.WriteLine("Дата рождения: ");
+                            Console.WriteLine("Введите день: ");
+                            String dayEdit = Console.ReadLine();
+                            Console.WriteLine("Введите месяц: ");
+                            String monthEdit = Console.ReadLine();
+                            Console.WriteLine("Введите год: ");
+                            String yearEdit = Console.ReadLine();
+                            Student.EditStudent(fioEdit, groupEdit, new DateTime(Convert.ToInt32(yearEdit), Convert.ToInt32(monthEdit), Convert.ToInt32(dayEdit)), idEdit);
+                            Console.WriteLine("Студент изменён");
+                        }
+                    }
+                    else if (Choice == 8)
+                    {
+                        if (Student.ListStudent.Count == 0)
+                        {
+                            Console.WriteLine("Нет студентов\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Поиск студента: ");
+                            Console.WriteLine("Введите id студента: ");
+                            int idStudent = Convert.ToInt32(Console.ReadLine());
+                            Student.ExitID(idStudent);
+                        }
+                    }
+                    else if (Choice == 9)
+                    {
+                        Console.WriteLine("Выход");
+                        exit = true;
+                    }
+                    if (exit == true)
+                    {
+                        break;
+                    }
                 }
-                else if (a == 2)
-                {
-                    PrintStudent(al);
-                }
-                else if (a == 3)
-                {
-                    RemoveStudent(al);
-                }
-                else kones = false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!");
             }
 
-        }
-
-    
-        private static void RemoveStudent(ArrayList al)
-        {
-            Console.WriteLine("Введите фамилию:");
-            string findFIO = Console.ReadLine();
-            bool fd = false;
-            Student findSt = new Student();
-            foreach (var item in al)
-            {
-                Student st = (Student)item;
-                if (findFIO == st.FIO)
-                {
-                    findSt = st;
-                    al.Remove(st);
-                    fd = true;
-                    break;
-                }
-            }
-            if (fd) { Console.WriteLine(findSt.ToString()); }
-            else { Console.WriteLine("Студент не найден"); }
-        }
-        
-        
-
-        private static void PrintMessage()
-        {
-            Console.WriteLine("Добавления студента нажмите на 1");
-            Console.WriteLine("Получения списка нажмите на 2");
-            Console.WriteLine("Удаления студента нажмите на 3");
-            Console.WriteLine("Для выхода на 9");
-        }
-        
-
-        private static void PrintStudent(ArrayList al)
-        {
-            foreach (var item in al)
-            {
-                Student p = (Student)item;
-                Console.WriteLine(p.ToString());
-            }
-        }
-
-        private static void AddStudent(ArrayList al)
-        {
-            string fio; string age; string grupa;
-            Console.WriteLine("ФИО студента ");
-            fio = Console.ReadLine();
-            Console.WriteLine("Возраст: ");
-            age = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Группа: ");
-            grupa = Console.ReadLine();
-            al.Add(new Student { Age = age, FIO = fio, Gruppa = grupa });
         }
     }
 }
